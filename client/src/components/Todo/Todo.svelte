5d7c4190d7c750c685fac28a2335fc0c22ae375d
misha-mad <zakharov3010@yandex.ru>
2023-07-16 21:19:03 +0400

    style: config and run prettier

--- client/src/components/Todo/Todo.svelte
+++ client/src/components/Todo/Todo.svelte
@@ -1,59 +1,60 @@
 <script>
-	import {mutation, subscribe} from 'svelte-apollo'
-	import {gql} from '@apollo/client/core'
+  import {mutation, subscribe} from 'svelte-apollo'
+  import {gql} from '@apollo/client/core'
 
-	const GetTodos = gql`
-		subscription GetTodos {
-			todo {
-				id
-				name
-				isDone
-			}
-		}
-	`
+  const GetTodos = gql`
+    subscription GetTodos {
+      todo {
+        id
+        name
+        isDone
+      }
+    }
+  `
 
-	const AddTodo = gql`
-		mutation ($name: String!) {
-			insert_todo(objects: { name: $name }) {
-				returning {
-					id
-					name
-					isDone
-				}
-			}
-		}
-	`
+  const AddTodo = gql`
+    mutation ($name: String!) {
+      insert_todo(objects: {name: $name}) {
+        returning {
+          id
+          name
+          isDone
+        }
+      }
+    }
+  `
 
-	const todoOp = subscribe(GetTodos)
-	const todoAdd = mutation(AddTodo)
+  const todoOp = subscribe(GetTodos)
+  const todoAdd = mutation(AddTodo)
 
-	let name = ""
-	function addTodo() {
-		todoAdd({variables: {name}}).then((data) => {
-				console.log(data)
-				name = ""
-			})
-			.catch((error) => {
-				console.error(error)
-			})
-	}
+  let name = ''
+  function addTodo() {
+    todoAdd({variables: {name}})
+      .then((data) => {
+        console.log(data)
+        name = ''
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 </script>
 
 <div style="text-align:center">
-	<h2>Todoer</h2>
+  <h2>Todoer</h2>
 
-	<form on:submit|preventDefault={addTodo}>
-		<input placeholder="new todo" bind:value={name} />
-		<button type='submit'>Submit</button>
-	</form>
+  <form on:submit|preventDefault={addTodo}>
+    <input placeholder="new todo" bind:value={name} />
+    <button type="submit">Submit</button>
+  </form>
 
-	{#await $todoOp}
-		<p>.. loading</p>
-	{:then data}
-		{#each data.data?.todo ?? [] as todo}
-			<p class:done={todo.isDone}>{todo.name}</p>
-		{/each}
-	{:catch error}
-		{error}
-	{/await}
+  {#await $todoOp}
+    <p>.. loading</p>
+  {:then data}
+    {#each data.data?.todo ?? [] as todo}
+      <p class:done={todo.isDone}>{todo.name}</p>
+    {/each}
+  {:catch error}
+    {error}
+  {/await}
 </div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,55 +1,59 @@
 <script>
-	import {ApolloClient, InMemoryCache, HttpLink, ApolloLink} from '@apollo/client/core'
-	import {setClient} from 'svelte-apollo'
-	import {PUBLIC_GRAPHQL_HTTP_API_URL, PUBLIC_GRAPHQL_WS_API_URL} from '$env/static/public'
-	import Todo from '../components/Todo/Todo.svelte'
-	import {GraphQLWsLink} from '@apollo/client/link/subscriptions'
-	import {setContext} from '@apollo/client/link/context'
-	import {createClient} from 'graphql-ws'
-	import {getMainDefinition} from '@apollo/client/utilities'
+  import {ApolloClient, InMemoryCache, HttpLink, ApolloLink} from '@apollo/client/core'
+  import {setClient} from 'svelte-apollo'
+  import {PUBLIC_GRAPHQL_HTTP_API_URL, PUBLIC_GRAPHQL_WS_API_URL} from '$env/static/public'
+  import Todo from '../components/Todo/Todo.svelte'
+  import {GraphQLWsLink} from '@apollo/client/link/subscriptions'
+  import {setContext} from '@apollo/client/link/context'
+  import {createClient} from 'graphql-ws'
+  import {getMainDefinition} from '@apollo/client/utilities'
 
-	const httpLink = new HttpLink({
-		uri: PUBLIC_GRAPHQL_HTTP_API_URL,
-	})
+  const httpLink = new HttpLink({
+    uri: PUBLIC_GRAPHQL_HTTP_API_URL,
+  })
 
-	const wsLink = new ApolloLink((operation) =>
-		new GraphQLWsLink(
-			createClient({
-				lazy: true,
-				connectionParams: {headers: operation.getContext().headers},
-				lazyCloseTimeout: 10000,
-				retryAttempts: 10,
-				url: PUBLIC_GRAPHQL_WS_API_URL,
-			}),
-		).request(operation),
-	)
+  const wsLink = new ApolloLink((operation) =>
+    new GraphQLWsLink(
+      createClient({
+        lazy: true,
+        connectionParams: {headers: operation.getContext().headers},
+        lazyCloseTimeout: 10000,
+        retryAttempts: 10,
+        url: PUBLIC_GRAPHQL_WS_API_URL,
+      }),
+    ).request(operation),
+  )
 
-	const splitLink = setContext((_, linkParams) => {
-		return linkParams.accessToken ? {headers: {authorization: `Bearer ${linkParams.accessToken}`}} : {}
-	}).split(
-		({query}) => {
-			const definition = getMainDefinition(query)
-			return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-		},
+  const splitLink = setContext((_, linkParams) => {
+    return linkParams.accessToken ? {headers: {authorization: `Bearer ${linkParams.accessToken}`}} : {}
+  }).split(
+    ({query}) => {
+      const definition = getMainDefinition(query)
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+    },
 
-		wsLink,
-		httpLink,
-	)
+    wsLink,
+    httpLink,
+  )
 
-	const client = new ApolloClient({
-		link: splitLink,
-		cache: new InMemoryCache(),
-	})
+  const client = new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache(),
+  })
 
-	setClient(client)
+  setClient(client)
 </script>
 
-<style>
-  h5 {color: grey}
-  .centrify {text-align: center}
-</style>
-
 <div class="centrify">
-	<h5>Hello PUP!</h5>
-	<Todo />
+  <h5>Hello PUP!</h5>
+  <Todo />
 </div>
+
+<style>
+  h5 {
+    color: grey;
+  }
+  .centrify {
+    text-align: center;
+  }
+</style>

17c3aefb1de06a9f6ef47ca9fe441ab87a3b84d9
misha-mad <zakharov3010@yandex.ru>
2023-07-16 20:43:23 +0400

    feat(client+server): update docker-compose.yml, config hasura, add db, add table todo, add public role, download schema, add Todo component, config apollo client

---
+++ client/src/components/Todo/Todo.svelte
@@ -0,0 +1,60 @@
+<script>
+	import {query, mutation} from 'svelte-apollo'
+	import {gql} from '@apollo/client/core'
+
+	const GetTodos = gql`
+		query GetTodos {
+			todo {
+				id
+				name
+				isDone
+			}
+		}
+	`
+
+	const AddTodo = gql`
+		mutation ($name: String!) {
+			insert_todo(objects: { name: $name }) {
+				returning {
+					id
+					name
+					isDone
+				}
+			}
+		}
+	`
+
+	const todoOp = query(GetTodos)
+	const todoAdd = mutation(AddTodo)
+
+	let name = ""
+	function addTodo() {
+		todoAdd({variables: {name}}).then((data) => {
+				console.log(data)
+				name = ""
+				todoOp.refetch()
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}
+</script>
+
+<div style="text-align:center">
+	<h2>Todoer</h2>
+
+	<form on:submit|preventDefault={addTodo}>
+		<input placeholder="new todo" bind:value={name} />
+		<button type='submit'>Submit</button>
+	</form>
+
+	{#await $todoOp}
+		<p>.. loading</p>
+	{:then data}
+		{#each data.data?.todo ?? [] as todo}
+			<p class:done={todo.isDone}>{todo.name}</p>
+		{/each}
+	{:catch error}
+		{error}
+	{/await}
+</div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client/core'
+	import {setClient} from 'svelte-apollo'
+	import {PUBLIC_GRAPHQL_WS_API_URL} from '$env/static/public'
+	import Todo from '../components/Todo/Todo.svelte'
+
+	const client = new ApolloClient({
+		link: createHttpLink({uri: PUBLIC_GRAPHQL_WS_API_URL}),
+		cache: new InMemoryCache(),
+	})
+
+	setClient(client)
+</script>
+
+<style>
+  h5 {color: grey}
+  .centrify {text-align: center}
+</style>
+
+<div class="centrify">
+	<h5>Hello PUP!</h5>
+	<Todo />
+</div>

74306058ad22fd43539ab116767949af74c0ea04
misha-mad <zakharov3010@yandex.ru>
2023-11-12 22:05:36 +0400

    Improve UX of Todo application with layout and styling changes

--- client/src/lib/Todo/index.svelte
+++ client/src/lib/Todo/index.svelte
@@ -12,24 +12,26 @@
   }
 </script>
 
-<div class="text-center max-w-xs">
-  <h2 class="text-2xl font-bold mb-5">Todoer</h2>
+<div class="grid grid-rows-[auto_auto_1fr] gap-y-3 text-center w-4/6 lg:w-2/6 h-96 content-start">
+  <h2 class="text-2xl font-bold">Todoer</h2>
 
-  <form class="grid gap-y-3 mb-3" on:submit|preventDefault={addTodo}>
+  <form class="grid gap-y-3" on:submit|preventDefault={addTodo}>
     <input class="text-black p-1" placeholder="new todo" bind:value={name} />
-    <button class="border rounded-md" type="submit">Submit</button>
+    <button class="border rounded-md" type="submit">Add</button>
   </form>
 
-  <div class="max-h-60 overflow-y-scroll">
+  <div class="grid rounded-md border min-h-full">
     {#if !$todos}
-      <p>...loading</p>
+      <p class="place-self-center">...loading</p>
     {:else}
-      {#each $todos?.data?.todos ?? [] as todo, i}
-        <div class="grid grid-flow-col justify-start gap-x-2n">
-          <span>{($todos?.data?.todos.length ?? 0) - i}.</span>
-          <p class="text-ellipsis overflow-hidden" class:done={todo.isDone}>{todo.name}</p>
-        </div>
-      {/each}
+      <div class="place-items-start overflow-y-scroll p-2">
+        {#each $todos?.data?.todos ?? [] as todo, i}
+          <div class="grid grid-flow-col justify-start gap-x-2">
+            <span>{($todos?.data?.todos.length ?? 0) - i}.</span>
+            <p class="text-ellipsis overflow-hidden" class:done={todo.isDone}>{todo.name}</p>
+          </div>
+        {/each}
+      </div>
     {/if}
   </div>
 </div>
